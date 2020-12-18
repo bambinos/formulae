8cf250dd3733e851add4dec2e94fdba82e801487
Tomas Capretto <tomicapretto@gmail.com>
2020-12-18 15:33:13 -0300

    Add Resolver

---
+++ formulae/resolver.py
@@ -0,0 +1,25 @@
+class Resolver:
+
+    def __init__(self, expr):
+        self.expr = expr
+
+    def resolve(self):
+        self.expr.accept(self)
+
+    def visitGroupingExpr(self):
+        pass
+
+    def visitBinaryExpr(self):
+        pass
+
+    def visitUnaryExpr(self):
+        pass
+
+    def visitCallExpr(self):
+        pass
+
+    def visitVariableExpr(self):
+        pass
+
+    def visitLiteralExpr(self):
+        pass
