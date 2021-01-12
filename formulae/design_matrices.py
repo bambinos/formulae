69506f12ab570c84e2f55bb7b9f610fbf031e7b7
Tomas Capretto <tomicapretto@gmail.com>
2021-01-12 13:58:55 -0300

    Inheritance among classed for design matrices

--- formulae/design_matrices.py
+++ formulae/design_matrices.py
@@ -1,29 +1,43 @@
 class DesignMatrices:
     """Wraps ResponseVector FixedMatrix and RandomMatrix
+
+    Parameters
+    ----------
+
+    model : ModelTerms
+        The model description.
+    data: DataFrame or dict
+        The data object where we take values from.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, model, data):
+        self.model = model
+        self.response = ResponseVector(self.model.response, data)
+        self.fixed = FixedMatrix(self.model.terms, data)
+        self.random = RandomMatrix(self.model.random_terms, data)
 
 
 class Base:
     """A base class for ResponseVector, FixedMatrix and RandomMatrix
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, model, data):
+        self.model = model
+        self.data = data
+        self.evaluate()
+
+    def evaluate(self):
+        return NotImplemented
 
 
 class ResponseVector(Base):
     """Representation of the respose vector of a model
     """
 
-    def __init__(self, term):
-        self.term = term
-        self.y = None
-        self.name = None
+    def __init__(self, term, data):
+        super().__init__(term, data)
 
-    def _evaluate(self, data):
+    def evaluate(self):
         """Evaluates `self.term` inside the data mask provided by `data` and
         updates `self.y` and `self.name`
         """
@@ -34,7 +48,10 @@ class FixedMatrix(Base):
     """Representation of the design matrix for the fixed part of a model.
     """
 
-    def __init__(self, model_terms):
+    def __init__(self, term, data):
+        super().__init__(term, data)
+
+    def evaluate(self):
         pass
 
 
@@ -42,5 +59,8 @@ class RandomMatrix(Base):
     """Representation of the design matrix for the fixed part of a model.
     """
 
-    def __init__(self, model_terms):
+    def __init__(self, term, data):
+        super().__init__(term, data)
+
+    def evaluate(self):
         pass
