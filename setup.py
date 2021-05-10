e4934179bf5a865f764648c4a521724b207074e9
Tomas Capretto <tomicapretto@gmail.com>
2021-05-10 15:50:01 -0300

    Prepare release

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if "setuptools" in sys.modules:
 
 setup(
     name="formulae",
-    version="0.0.9",
+    version=__version__,
     description="An implementation of Wilkinson's formula language for statistical models à la lme4",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
