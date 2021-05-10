d16c7ccd2fefae73f6f7f05c1e3b71cc5ab58510
Tomás Capretto <tomicapretto@gmail.com>
2021-05-10 15:10:28 -0300

    Auto docs (#25)
    * Add codecov badge to index.rst
    
    * Attempt to automate documentation publish
    
    * install formulae in workflow
    
    * checkout@v2
    
    * pre build command
    
    * insert path in conf.py
    
    * delete token line
    
    * add token
    
    * Workflow run on push now

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,10 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath('../'))
+
 from formulae import __version__
 
-sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
