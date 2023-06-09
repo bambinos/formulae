0ec4fdf17949e9e59cfb6a4278612674f47f9e12
Tomás Capretto <tomicapretto@gmail.com>
2023-06-09 14:35:37 -0300

    Allow evaluations on new data with new categories (#96)

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,11 +6,13 @@ from formulae.config import Config
 def test_config():
     config = Config()
 
-    assert config["EVAL_NEW_CATEGORIES"] == "error"
-    assert config.EVAL_NEW_CATEGORIES == "error"
+    assert config["EVAL_UNSEEN_CATEGORIES"] == "error"
+    assert config.EVAL_UNSEEN_CATEGORIES == "error"
 
-    with pytest.raises(ValueError, match="anything is not a valid value for 'EVAL_NEW_CATEGORIES'"):
-        config.EVAL_NEW_CATEGORIES = "anything"
+    with pytest.raises(
+        ValueError, match="anything is not a valid value for 'EVAL_UNSEEN_CATEGORIES'"
+    ):
+        config.EVAL_UNSEEN_CATEGORIES = "anything"
 
     with pytest.raises(KeyError, match="'DOESNT_EXIST' is not a valid configuration option"):
         config.DOESNT_EXIST = "anything"
